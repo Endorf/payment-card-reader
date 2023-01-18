48911ddd4a1bfba1cfcd25aa0f705d0972c0dc53
dmytrokosolapenko <dmitriy.kosolapenko@gmail.com>
2023-01-18 10:43:15 +0200

    configure nfc module project

---
+++ nfc/src/main/java/com/paymentcardreader/reader/nfc/NFCCardReader.kt
@@ -0,0 +1,13 @@
+package com.paymentcardreader.reader.nfc
+
+import android.app.Activity
+import android.content.Intent
+
+class NFCCardReader(activity: Activity) {
+
+    fun onNewIntent(intent: Intent?) {}
+
+    fun enableForegroundDispatch() {}
+
+    fun disableForegroundDispatch() {}
+}
