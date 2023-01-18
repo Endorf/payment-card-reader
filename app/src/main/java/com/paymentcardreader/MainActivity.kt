f65623c6ca6d139edf5bd8e2b998d17d2e37d8d0
dmytrokosolapenko <dmitriy.kosolapenko@gmail.com>
2023-01-18 10:43:45 +0200

    initiate cardReader calls

--- app/src/main/java/com/paymentcardreader/MainActivity.kt
+++ app/src/main/java/com/paymentcardreader/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.paymentcardreader
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.paymentcardreader.reader.nfc.NFCCardReader
 
 class MainActivity : AppCompatActivity() {
+
+    private val nfcCardReader: NFCCardReader by lazy { NFCCardReader(this) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        nfcCardReader.onNewIntent(intent)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        nfcCardReader.enableForegroundDispatch()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        nfcCardReader.disableForegroundDispatch()
     }
 }
