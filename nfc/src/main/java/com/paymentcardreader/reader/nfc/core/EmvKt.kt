20eb747a856d5e0de5679eb17c06e0bef9feda9d
dmytrokosolapenko <dmitriy.kosolapenko@gmail.com>
2023-01-23 16:29:26 +0200

    readTagLength

--- nfc/src/main/java/com/paymentcardreader/reader/nfc/core/EmvKt.kt
+++ nfc/src/main/java/com/paymentcardreader/reader/nfc/core/EmvKt.kt
@@ -31,11 +31,39 @@ private fun ByteArrayInputStream.readTagIdBytes(): ByteArray? {
     return outputStream.toByteArray()
 }
 
-fun Int.matchBitByBitIndex(pBitIndex: Int): Boolean {
+internal fun Int.matchBitByBitIndex(pBitIndex: Int): Boolean {
     return if (pBitIndex in 0..31) {
         (this and (1 shl pBitIndex)) != 0
     } else {
         false
     }
 }
+
+private fun ByteArrayInputStream.readTagLength(): Int {
+    var tagLength: Int = read()
+
+    if (tagLength.isOneByteLength().not()) {
+        val length = tagLength and 127 // turn off 8th bit
+        tagLength = 0
+        for (i in 0 until length) {
+            val firstByte = tagLength shl 8
+            val secondByte: Int = read()
+
+            tagLength = firstByte or secondByte
+        }
+    }
+    return tagLength
+}
+
+/**
+ * When bit b8 of the most significant byte of the length field is set to 0,
+ * the length field consists of only one byte.
+ * Bits b7 to b1 code the number of bytes of the value field. The length field is within the range 1 to 127.
+ *
+ * When bit b8 of the most significant byte of the length field is set to 1,
+ * the subsequent bits b7 to b1 of the most significant byte code the number of subsequent bytes in the length field.
+ * The subsequent bytes code an integer representing the number of bytes in the value field.
+ * Two bytes are necessary to express up to 255 bytes in the value field.
+ */
+private fun Int.isOneByteLength(): Boolean = (this shr 8) == 0
 
